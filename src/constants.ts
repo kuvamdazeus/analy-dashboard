6fce695b2623fb31c8f416d80a0f28b55aaa2ceb
Kuvam Bhardwaj <kuvamyash@gmail.com>
2023-02-20 01:20:10 +0530

    console log

--- src/constants.ts
+++ src/constants.ts
@@ -17,5 +17,3 @@ export const REDIRECT_URI =
 
 export const GITHUB_OAUTH_URI = (state = "") =>
   `https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GH_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=read:user,user:email&state=${state}`;
-
-console.log(REDIRECT_URI);
