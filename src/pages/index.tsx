e3ecd382be92118a28c150247adcd1aec0d8cf05
Kuvam Bhardwaj <kuvamyash@gmail.com>
2023-02-24 00:27:51 +0530

    - dashboard redirect instead of OAuth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,18 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 import { GITHUB_OAUTH_URI } from "@/constants";
-import { useCookies } from "react-cookie";
+import type { GetServerSideProps, NextApiRequest } from "next";
+import { userCookie } from "@/server/utils/cookies";
 
-export default function Index() {
-  const [cookies] = useCookies(["user"]);
+interface Props {
+  loggedIn: boolean;
+}
 
+export default function Index({ loggedIn }: Props) {
   const router = useRouter();
 
   const isRedirect = !!router.query.redirect;
-  const canRedirect = !!cookies.user;
+  const canRedirect = loggedIn;
 
   useEffect(() => {
     if (isRedirect) {
@@ -18,15 +21,25 @@ export default function Index() {
   }, [isRedirect]);
 
   return (
-    <div className="">
-      <p className="text-3xl">Landing page</p>
+    <section className="flex h-screen flex-col items-center justify-center">
+      <p className="mb-5 text-3xl font-bold">Analy Dashboard</p>
 
       <a
         href={canRedirect ? "/dashboard" : GITHUB_OAUTH_URI()}
-        className="m-5 border border-red-500"
+        className="rounded bg-gray-600 px-12 py-2 text-lg text-white"
       >
         Login with Github
       </a>
-    </div>
+    </section>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const userId = userCookie.get(req as NextApiRequest);
+
+  return {
+    props: {
+      loggedIn: userId,
+    },
+  };
+};
